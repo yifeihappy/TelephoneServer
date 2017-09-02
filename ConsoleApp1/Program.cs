50c854122270b4d557bdc2081c0992c0f253bc14
yifeihappy <1477675362@qq.com>
2017-09-02 15:06:05 +0800

    complete the progam to show accelerometer data changes on console on real time

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -19,38 +19,55 @@ namespace ConsoleApp1
         {
             string name = Dns.GetHostName();
             IPAddress[] ipadrlist = Dns.GetHostAddresses(name);
+            Console.WriteLine("server ip:{0}", ipadrlist[1]);
             serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            serverSocket.Bind(new IPEndPoint(ipadrlist[0], myPort));
+            serverSocket.Bind(new IPEndPoint(ipadrlist[1], myPort));
             serverSocket.Listen(10);
             Console.WriteLine("启动监听{0}成功", serverSocket.LocalEndPoint.ToString());
-            Thread myThread = new Thread(ListenClientConnect);
-            //foreach (IPAddress ipa in ipadrlist)
-            //{
-            //    if (ipa.AddressFamily == AddressFamily.InterNetwork)
-            //        Console.WriteLine(ipa.ToString());
-            //}
-            //Console.ReadKey();
-
+            Thread socketThread = new Thread(ListenClientConnect);
+            socketThread.IsBackground = true;
+            socketThread.Start();
+            Console.ReadKey();
+            //socketThread.Abort();
         }
 
         private static void ListenClientConnect()
         {
-            Socket clientSocket = serverSocket.Accept();
             while (true)
+            {
+                Socket clientSocket = serverSocket.Accept();
+                Thread recvThread = new Thread(recvData);
+                recvThread.IsBackground = true;
+                recvThread.Start(clientSocket);
+            }
+        }
+        private static void recvData(object s)
+        {
+            Socket clientSocket = (Socket)s;
+            while(true)
             {
                 try
                 {
+                    DateTime curTime = System.DateTime.Now;
+                    int minute = curTime.Minute;
+                    int second = curTime.Second;
+                     result = new byte[1024];
                     int receiveNumber = clientSocket.Receive(result);
-                    Console.WriteLine("接收客户端{0}消息{1}", clientSocket.RemoteEndPoint.ToString()
+                    if(receiveNumber<=0)break;
+                    Console.WriteLine("receiveNUm:{0}",receiveNumber);
+                    Console.WriteLine(""+minute+":"+second+
+                        "/接收客户端{0}消息{1}", clientSocket.RemoteEndPoint.ToString()
                         , Encoding.ASCII.GetString(result, 0, receiveNumber));
                 }catch(Exception ex)
-                {
+                { 
                     Console.WriteLine(ex.Message);
                     clientSocket.Shutdown(SocketShutdown.Both);
                     clientSocket.Close();
                     break;
                 }
             }
+            
+            
         }
     }
 }
