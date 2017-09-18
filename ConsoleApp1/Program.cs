7ab00f620a5071cd35868415649fc4f6168189a5
yifeihappy <yifeihappy@YIFEIHAPPY-PC>
2017-09-18 20:43:58 +0800

    ConcurrentQueue

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -14,7 +15,7 @@ namespace ConsoleApp1
         private static byte[] result = new byte[1024];
         private static int myPort = 30000;//port
         static Socket serverSocket;
-
+        public static ConcurrentQueue<SensorDataItem> sensorDataQueue = new ConcurrentQueue<SensorDataItem>();
         static void Main(string[] args)
         {
             string name = Dns.GetHostName();
@@ -54,10 +55,30 @@ namespace ConsoleApp1
                      result = new byte[1024];
                     int receiveNumber = clientSocket.Receive(result);
                     if(receiveNumber<=0)break;
-                    Console.WriteLine("receiveNUm:{0}",receiveNumber);
-                    Console.WriteLine(""+minute+":"+second+
-                        "/接收客户端{0}消息{1}", clientSocket.RemoteEndPoint.ToString()
-                        , Encoding.ASCII.GetString(result, 0, receiveNumber));
+                    Console.WriteLine(minute+":"+second+"  receiveNUm:{0}",receiveNumber);
+                    //Console.WriteLine(""+minute+":"+second+
+                    //    "/接收客户端{0}消息{1}", clientSocket.RemoteEndPoint.ToString()
+                    //    , Encoding.ASCII.GetString(result, 0, receiveNumber));
+                    String str = Encoding.ASCII.GetString(result, 0, receiveNumber);
+                    String[] strArr = str.Split('\n');
+                    for(int i=0;i<strArr.Length;i++)
+                    {
+                        if (strArr[i].Length == 0) continue;
+                        string[] sArr = strArr[i].Split(',');
+                        Console.WriteLine("sARR.L={0}", sArr.Length);
+                        if (sArr.Length != 5) continue;
+                        SensorDataItem sditem = new SensorDataItem();
+                        sditem.Type = Convert.ToInt32(sArr[0]);
+                        sditem.X = Convert.ToDouble(sArr[1]);
+                        sditem.Y = Convert.ToDouble(sArr[2]);
+                        sditem.Z = Convert.ToDouble(sArr[3]);
+                        sditem.Timestamp = Convert.ToUInt64(sArr[4]);
+
+                        sensorDataQueue.Enqueue(sditem);
+                            
+                            
+                    }
+
                 }catch(Exception ex)
                 { 
                     Console.WriteLine(ex.Message);
---
+++ ConsoleApp1/SensorDataItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class SensorDataItem
+    {
+        private ulong timestamp;
+        private double x;
+        private double y;
+        private double z;
+        private int type;
+
+        public ulong Timestamp { get => timestamp; set => timestamp = value; }
+        public double X { get => x; set => x = value; }
+        public double Y { get => y; set => y = value; }
+        public double Z { get => z; set => z = value; }
+        public int Type { get => type; set => type = value; }
+    }
+}
