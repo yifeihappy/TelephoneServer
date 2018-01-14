1f6a5efa96e6abe89f5c51eb41ab507554d27db5
yifeihappy <yifeihappy@YIFEIHAPPY-PC>
2018-01-14 21:54:17 +0800

    自动获取IPv4

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,6 +8,7 @@ using System.Net.Sockets;
 using System.ServiceModel;
 using System.ServiceModel.Description;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -24,6 +25,8 @@ namespace TelephoneSensorService
         public static ConcurrentQueue<SensorDataItemXD> sensorDataQueue = new ConcurrentQueue<SensorDataItemXD>();
         static void Main(string[] args)
         {
+
+
             ServiceHost host = new ServiceHost(typeof(SensorDataService1));
             host.Open();
 
@@ -35,14 +38,15 @@ namespace TelephoneSensorService
 
             //socket listent
             string name = Dns.GetHostName();
-            IPAddress[] ipadrlist = Dns.GetHostAddresses(name);
-            for (int j = 0; j < ipadrlist.Length; j++)
-            {
-                Debug.WriteLine("server ip[{0}]:{1}", j, ipadrlist[j]);
-            }
+            //IPAddress[] ipadrlist = Dns.GetHostAddresses(name);
+            //for (int j = 0; j < ipadrlist.Length; j++)
+            //{
+            //    Debug.WriteLine("server ip[{0}]:{1}", j, ipadrlist[j]);
+            //}
 
             serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            serverSocket.Bind(new IPEndPoint(ipadrlist[1], myPort));
+            //serverSocket.Bind(new IPEndPoint(ipadrlist[3], myPort));
+            serverSocket.Bind(new IPEndPoint(IPAddress.Parse(GetLocalIP()), myPort));
             serverSocket.Listen(10);
             Console.WriteLine("启动监听{0}成功", serverSocket.LocalEndPoint.ToString());
             Thread socketThread = new Thread(ListenClientConnect);
@@ -59,7 +63,7 @@ namespace TelephoneSensorService
 
             Console.WriteLine("Close......");
 
-            //socketThread.Abort();
+            socketThread.Abort();
         }
 
         private static void ListenClientConnect()
@@ -145,6 +149,99 @@ namespace TelephoneSensorService
                     clientSocket.Close();
                     break;
                 }
+            }
+        }
+
+        /// <summary>  
+        /// 获取当前使用的IP  
+        /// </summary>  
+        /// <returns></returns>  
+        public static string GetLocalIP()
+        {
+            string result = RunApp("route", "print", true);
+            Match m = Regex.Match(result, @"0.0.0.0\s+0.0.0.0\s+(\d+.\d+.\d+.\d+)\s+(\d+.\d+.\d+.\d+)");
+            if (m.Success)
+            {
+                return m.Groups[2].Value;
+            }
+            else
+            {
+                try
+                {
+                    System.Net.Sockets.TcpClient c = new System.Net.Sockets.TcpClient();
+                    c.Connect("www.baidu.com", 80);
+                    string ip = ((System.Net.IPEndPoint)c.Client.LocalEndPoint).Address.ToString();
+                    c.Close();
+                    return ip;
+                }
+                catch (Exception)
+                {
+                    return null;
+                }
+            }
+        }
+
+        /// <summary>  
+        /// 获取本机主DNS  
+        /// </summary>  
+        /// <returns></returns>  
+        public static string GetPrimaryDNS()
+        {
+            string result = RunApp("nslookup", "", true);
+            Match m = Regex.Match(result, @"\d+\.\d+\.\d+\.\d+");
+            if (m.Success)
+            {
+                return m.Value;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        /// <summary>  
+        /// 运行一个控制台程序并返回其输出参数。  
+        /// </summary>  
+        /// <param name="filename">程序名</param>  
+        /// <param name="arguments">输入参数</param>  
+        /// <returns></returns>  
+        public static string RunApp(string filename, string arguments, bool recordLog)
+        {
+            try
+            {
+                if (recordLog)
+                {
+                    Trace.WriteLine(filename + " " + arguments);
+                }
+                Process proc = new Process();
+                proc.StartInfo.FileName = filename;
+                proc.StartInfo.CreateNoWindow = true;
+                proc.StartInfo.Arguments = arguments;
+                proc.StartInfo.RedirectStandardOutput = true;
+                proc.StartInfo.UseShellExecute = false;
+                proc.Start();
+
+                using (System.IO.StreamReader sr = new System.IO.StreamReader(proc.StandardOutput.BaseStream, Encoding.Default))
+                {
+
+                    //上面标记的是原文，下面是我自己调试错误后自行修改的  
+                    Thread.Sleep(100);           //貌似调用系统的nslookup还未返回数据或者数据未编码完成，程序就已经跳过直接执行  
+                                                 //txt = sr.ReadToEnd()了，导致返回的数据为空，故睡眠令硬件反应  
+                    if (!proc.HasExited)         //在无参数调用nslookup后，可以继续输入命令继续操作，如果进程未停止就直接执行  
+                    {                            //txt = sr.ReadToEnd()程序就在等待输入，而且又无法输入，直接掐住无法继续运行  
+                        proc.Kill();
+                    }
+                    string txt = sr.ReadToEnd();
+                    sr.Close();
+                    if (recordLog)
+                        Trace.WriteLine(txt);
+                    return txt;
+                }
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine(ex);
+                return ex.Message;
             }
         }
 
